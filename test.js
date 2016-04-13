716f30b96b28bb8f1667c4c60f3c9c514d6bbbea
F10CH <flochy@obside.fr>
2016-04-13 11:52:21 +0200

    Update test.js

--- test.js
+++ test.js
@@ -4,4 +4,4 @@ var opt = O.getOpt(process.argv, 2)
 console.log("tested params",Array.from(opt.it()))
 console.log("opt.test('-a') = ",opt.test('-a'))
 
-O.testOpt('--param',(e,i,p)=>{ console.log('parameters for --param at',i,':', p) },2)
+O.test('--param',(e,i,p)=>{ console.log('parameters for --param at',i,':', p) },2)
